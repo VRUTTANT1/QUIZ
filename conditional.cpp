d23733e88e0c2ad74b2ddd80181a25b98a08421a
VRUTTANT1 <vruttant54321@gmail.com>
2024-12-18 09:42:33 +0530

    Initial commit

---
+++ conditional.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int score;
+cin>>score;
+
+if (score>400){
+    cout<<"India wins";
+}
+
+else{
+    cout<<"India loose";
+}
+
+return 0;
+}
